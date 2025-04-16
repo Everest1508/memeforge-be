ca999648caf8f09a69468f245ba216310cb5d34f
Everest1508 <riteshmahale15@gmail.com>
2025-04-16 22:23:58 +0530

    feat: add image preview functionality to ImageAdmin in admin panel

--- stickers/admin.py
+++ stickers/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import Image, ImageCategory, TeamMember
 from django_summernote.admin import SummernoteModelAdmin
+from django.utils.html import format_html
+
 
 
 @admin.register(ImageCategory)
@@ -11,9 +13,17 @@ class ImageCategoryAdmin(ImportExportModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(ImportExportModelAdmin):
-    list_display = ('title', 'category', 'uploaded_at')
+    list_display = ('title', 'category', 'uploaded_at', 'image_preview')
     list_filter = ('category',)
     search_fields = ('title', 'short_description')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px; max-width: 300px;" />', obj.image.url)
+        return "No image available"
+    
+    image_preview.short_description = "Image Preview"
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(ImportExportModelAdmin, SummernoteModelAdmin):
