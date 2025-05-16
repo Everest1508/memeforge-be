08f7e125d6f6012dead4de51e9480f2a833fcff0
Everest1508 <riteshmahale15@gmail.com>
2025-05-16 12:20:26 +0530

    fix: update submission_count method in MemeforgeUserAdmin to directly query UserSubmission for accurate count

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,8 @@ class MemeforgeUserAdmin(admin.ModelAdmin):
         return qs.annotate(sub_count=Count('submissions'))
 
     def submission_count(self, obj):
-        return obj.sub_count
+        from submissions.models import UserSubmission
+        return UserSubmission.objects.filter(memeforge_user=obj).count()
     submission_count.short_description = 'Submissions'
 
     def profile_picture_preview(self, obj):
